31e4b0bb737ca058d4283975b63e2b0a135b4fe5
cavy111 <calvindube.cd@gmail.com>
2026-01-08 23:37:55 +0200

    add the ui

--- app.js
+++ app.js
@@ -14,16 +14,40 @@ function getHumanChoice(){
 
 
 
-function playGame(){
+// function playGame(){
 
-    let humanScore = 0
-    let computerScore = 0
+    
 
-    function playRound(humanChoice,computerChoice){
+    
+
+// // for (let i=0; i<5; i++){
+// //     console.log('ROUND '.concat(`${i+1}`));
+    
+// //     console.log(playRound(getHumanChoice(),getComputerChoice()))
+// //     console.log('computer: '+computerScore, 'you: '+humanScore);
+    
+// // }
+
+// }
+
+const comp_choice_display = document.querySelector('.comp-choice')
+const user_choice_display = document.querySelector('.user-choice')
+const round_statement_display = document.querySelector('.round_statement_display')
+const user_score_display = document.querySelector('.user-score')
+const comp_score_display = document.querySelector('.comp-score')
+
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice,computerChoice){
         let statement =`your choice is ${humanChoice}
+
         computer choice is ${computerChoice}`
+        user_choice_display.textContent=humanChoice
+        comp_choice_display.textContent=computerChoice
         let lower_case_human_choice = humanChoice.toLowerCase()
         if (lower_case_human_choice === computerChoice){
+            round_statement_display.textContent = `its a tie you both picked ${humanChoice}`
             return statement.concat(`
                 its a tie you both picked ${humanChoice}`)
         }
@@ -31,11 +55,37 @@ function playGame(){
         (lower_case_human_choice === 'scissors' && computerChoice === 'paper') || 
         (lower_case_human_choice === 'paper' && computerChoice === 'rock')){
             humanScore+=1
+            user_score_display.textContent=humanScore
+            if (humanScore >=5){
+                alert(`Congratulations You won. Final score you: ${humanScore} - comp: ${computerScore}`)
+                humanScore = 0
+                computerScore = 0
+                comp_choice_display.textContent=''
+                user_score_display.textContent=''
+                round_statement_display.textContent=''
+                comp_score_display.textContent=0
+                user_score_display.textContent=0
+                return
+            }
+            round_statement_display.textContent = `you win ${humanChoice} beats ${computerChoice}`
             return statement.concat(`
                 you win ${humanChoice} beats ${computerChoice}`)
 
         }else{
             computerScore+=1
+            comp_score_display.textContent=computerScore
+            if (computerScore >=5){
+                alert(`Better Luck next time You lost. Final score you: ${humanScore} - comp: ${computerScore}`)
+                humanScore = 0
+                computerScore = 0
+                comp_choice_display.textContent=''
+                user_score_display.textContent=''
+                round_statement_display.textContent=''
+                comp_score_display.textContent=0
+                user_score_display.textContent=0
+                return
+            }
+             round_statement_display.textContent = `you lose ${computerChoice} beats ${humanChoice}`
             return statement.concat(`
                 you lose ${computerChoice} beats ${humanChoice}`)
         }
@@ -46,15 +96,14 @@ function playGame(){
     */    
 }
 
-for (let i=0; i<5; i++){
-    console.log('ROUND '.concat(`${i+1}`));
-    
-    console.log(playRound(getHumanChoice(),getComputerChoice()))
-    console.log('computer: '+computerScore, 'you: '+humanScore);
-    
-}
+// playGame()
 
-}
+const btns = document.querySelectorAll('button')
+btns.forEach(btn=>{
+    btn.addEventListener('click',()=>{
+        console.log(playRound(btn.textContent,getComputerChoice()))
+    })    
+})
 
-playGame()
+
 
